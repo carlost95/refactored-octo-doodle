d889337fff26e122743f6cb1021ed9ab706e2326
carlos tejada <carlosanibaltejada@gmail.com>
2020-09-09 17:44:02 -0300

    feat: se agrega metodos validacion y metodos de exportacion a bancos

---
+++ src/app/modelo/BancoExcel.ts
@@ -0,0 +1,10 @@
+export class BancoExcel {
+    id: number;
+    nombre: string;
+    abreviatura: string;
+    constructor(id: number, nombre: string, abreviatura: string) {
+        this.id = id;
+        this.nombre = nombre;
+        this.abreviatura = abreviatura;
+    }
+}
