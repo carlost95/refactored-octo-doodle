dd2ea80f43360a82aa884f0b11b187cb621fcee1
carlos tejada <carlosanibaltejada@gmail.com>
2020-10-19 17:38:33 -0300

    refactor: delete de metodos sin funcionar y correcion de modulos

--- src/app/compras/compras.module.ts
+++ src/app/compras/compras.module.ts
@@ -9,13 +9,11 @@ import {SubMenuComponent} from './sub-menu/sub-menu.component';
 import {ListarProveedorComponent} from './listar-proveedor/listar-proveedor.component';
 import {ListarArticulosComponent} from './listar-articulos/listar-articulos.component';
 import {AgregarProveedorComponent} from './agregar-proveedor/agregar-proveedor.component';
-import {ModificarArticuloComponent} from './modificar-articulo/modificar-articulo.component';
 import {AgregarArticuloComponent} from './agregar-articulo/agregar-articulo.component';
 import {ListarPedidoComponent} from './listar-pedido/listar-pedido.component';
 import {AgregarPedidoComponent} from './agregar-pedido/agregar-pedido.component';
 import {ConsultarPedidoComponent} from './consultar-pedido/consultar-pedido.component';
 import {ExcelExportService} from '../service/excel-export.service';
-import {AgregarMarcaComponent} from '../abm-compras/agregar-marca/agregar-marca.component';
 import {AbmComprasModule} from '../abm-compras/abm-compras.module';
 import {MaterialModule} from '../material/material.module';
 import {PaginacionPipe} from './paginacion.pipe';
@@ -30,7 +28,6 @@ import {CustomMatPaginatorIntl} from './paginacion-es';
     ListarProveedorComponent,
     ListarArticulosComponent,
     AgregarProveedorComponent,
-    ModificarArticuloComponent,
     AgregarArticuloComponent,
     ListarPedidoComponent,
     AgregarPedidoComponent,
