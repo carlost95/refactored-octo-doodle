093fc0b70f7c78a95ce94ab6df788b8e044ad0b4
carlos tejada <carlosanibaltejada@gmail.com>
2020-10-27 20:47:20 -0300

    feat: modificacion de servicios de seguridad para crear y logear user

--- src/app/models/jwt-dto.ts
+++ src/app/models/jwt-dto.ts
@@ -1,2 +1,8 @@
 export class JwtDTO {
+  token: string;
+  type: string;
+  nombreUsuaario: string;
+  authorities: string[];
+
+
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NewUsuario} from '../models/new-usuario';
+import {Observable} from 'rxjs';
+import {LoginUsuario} from '../models/login-usuario';
+import {JwtDTO} from '../models/jwt-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  authURL = 'http://localhost:8081/auth/';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public nuevo(newUsuario: NewUsuario): Observable<any> {
+    return this.httpClient.post<any>(this.authURL + 'nuevo', newUsuario);
+  }
+
+  public login(loginUsuario: LoginUsuario): Observable<JwtDTO> {
+    return this.httpClient.post<JwtDTO>(this.authURL + 'login', loginUsuario);
+  }
 }
