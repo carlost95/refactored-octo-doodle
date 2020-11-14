fc64df666ec9e22f12db76265cbc4b12272e5d23
carlos tejada <carlosanibaltejada@gmail.com>
2020-11-13 22:34:02 -0300

    feat: abm-ventas modificacion de modulos para permisos

--- src/app/pages/abm-ventas/abm-ventas-routing.module.ts
+++ src/app/pages/abm-ventas/abm-ventas-routing.module.ts
@@ -1,23 +1,31 @@
-import { AgregarDistritoComponent } from "./distrito/agregar-distrito/agregar-distrito.component";
-import { AbmVentasComponent } from "./abm-ventas.component";
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import {DepartamentoComponent} from "./departamento/departamento.component";
-import {DistritoComponent} from "./distrito/distrito.component";
+import {AbmVentasComponent} from './abm-ventas.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DepartamentoComponent} from './departamento/departamento.component';
+import {DistritoComponent} from './distrito/distrito.component';
+import {ProdGuardService as guard} from '../../guars/prod-guard.service';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: AbmVentasComponent,
     children: [
-      { path: "departamentos", component: DepartamentoComponent},
-      { path: "distritos", component: DistritoComponent },
+      {
+        path: 'departamentos', component: DepartamentoComponent,
+        canActivate: [guard], data: {expectedRol: ['admin', 'gerente', 'user']}
+      },
+      {
+        path: 'distritos', component: DistritoComponent,
+        canActivate: [guard], data: {expectedRol: ['admin', 'gerente', 'user']}
+      },
 
     ]
   }
 ];
+
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AbmVentasRoutingModule {}
+export class AbmVentasRoutingModule {
+}
