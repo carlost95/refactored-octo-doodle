47dabb8d5c0fa2ee18f3ed32af7a7ff0ac3ee647
carlos tejada <carlosanibaltejada@gmail.com>
2020-11-13 23:46:56 -0300

    feat: agregado de filter y permisos en departamentos

--- src/app/pages/abm-ventas/departamento/departamento.component.ts
+++ src/app/pages/abm-ventas/departamento/departamento.component.ts
@@ -1,13 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {DepartamentosService} from "../../../service/departamentos.service";
-import {Departamento} from "../../../models/Departamento";
-import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {AgregarClienteComponent} from "../../ventas/clientes/agregar-cliente/agregar-cliente.component";
-import {AgregarDepartamentoComponent} from "./agregar-departamento/agregar-departamento.component";
-import {SnackConfirmComponent} from "../../../shared/snack-confirm/snack-confirm.component";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {Cliente} from "../../../models/Cliente";
-import {ConfirmModalComponent} from "../../../shared/confirm-modal/confirm-modal.component";
+import {DepartamentosService} from '../../../service/departamentos.service';
+import {Departamento} from '../../../models/Departamento';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {AgregarDepartamentoComponent} from './agregar-departamento/agregar-departamento.component';
+import {SnackConfirmComponent} from '../../../shared/snack-confirm/snack-confirm.component';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {ConfirmModalComponent} from '../../../shared/confirm-modal/confirm-modal.component';
+import {TokenService} from '../../../service/token.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-departamento',
@@ -17,36 +17,59 @@ import {ConfirmModalComponent} from "../../../shared/confirm-modal/confirm-modal
 export class DepartamentoComponent implements OnInit {
 
   departamentos: Departamento[];
+  departamentosFilter: Departamento[];
   private update: Departamento;
-  private consulting: boolean = false;
+  consulting = false;
+  busqueda: string = null;
+
+  isLogged = false;
+  roles: string[];
+  isAdmin = false;
+  isGerente = false;
 
   constructor(
     private departamentoService: DepartamentosService,
     public matDialog: MatDialog,
-    private _snackBar: MatSnackBar
+    // tslint:disable-next-line:variable-name
+    private _snackBar: MatSnackBar,
+    private router: Router,
+    private tokenService: TokenService
   ) {
   }
 
   ngOnInit(): void {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+    this.roles = this.tokenService.getAuthorities();
+    this.roles.forEach(rol => {
+      if (rol === 'ROLE_ADMIN') {
+        this.isAdmin = true;
+      } else if (rol === 'ROLE_GERENTE') {
+        this.isGerente = true;
+      }
+    });
     this.getData();
   }
 
-  getData() {
+  getData(): void {
     this.departamentoService.getAll().subscribe(resp => {
       this.departamentos = resp.data;
-      console.log(resp.data)
+      this.departamentosFilter = this.departamentos;
+      console.log(resp.data);
     });
   }
 
-  showModal(departamento: Departamento) {
+  showModal(departamento: Departamento): void {
     const dialogConfig = new MatDialogConfig();
-    // The user can't close the dialog by clicking outside its body
     dialogConfig.disableClose = true;
-    dialogConfig.id = "modal-component";
+    dialogConfig.id = 'modal-component';
     dialogConfig.height = '300px';
     dialogConfig.width = '350px';
     dialogConfig.data = {
-      message: 'Desea cambiar estado?',
+      message: '¿Desea cambiar estado?',
       title: 'Cambio estado',
       state: departamento.estado
     };
@@ -55,36 +78,47 @@ export class DepartamentoComponent implements OnInit {
       if (result.state) {
         this.departamentoService.changeStatus(departamento.id).subscribe(result => {
           this.getData();
-        })
+        });
       } else {
         this.getData();
       }
     });
   }
 
-  nuevo() {
+  filtrarDepartamento(event: any): void {
+    this.busqueda = this.busqueda.toLowerCase();
+    this.departamentosFilter = this.departamentos;
+    if (this.busqueda !== null) {
+      this.departamentosFilter = this.departamentos.filter(item => {
+        const nombre = item.nombre.toLowerCase().indexOf(this.busqueda) !== -1;
+        const abreviatura = item.abreviatura.toLowerCase().indexOf(this.busqueda) !== -1;
+        return nombre || abreviatura;
+      });
+    }
+  }
+
+  nuevo(): void {
     this.consulting = false;
     this.update = undefined;
-    this.openDialog()
+    this.openDialog();
 
   }
 
-  consultar(departamento: Departamento) {
+  consultar(departamento: Departamento): void {
     this.consulting = true;
     this.update = departamento;
-    this.openDialog()
+    this.openDialog();
   }
 
-  editar(departamento: Departamento) {
+  editar(departamento: Departamento): void {
     this.consulting = false;
     this.update = departamento;
-    this.openDialog()
+    this.openDialog();
   }
 
-  volver() {
-    window.history.back()
+  volver(): void {
+    this.router.navigate(['abm-ventas']);
   }
-
 
   openDialog(): void {
     const dialogConfig = new MatDialogConfig();
@@ -103,14 +137,14 @@ export class DepartamentoComponent implements OnInit {
         this.openSnackBar(result);
       }
       this.getData();
-    })
+    });
   }
 
-  openSnackBar(msg: string) {
+  openSnackBar(msg: string): void {
     this._snackBar.openFromComponent(SnackConfirmComponent, {
       panelClass: ['error-snackbar'],
       duration: 5 * 1000,
-      data: msg
+      data: msg,
     });
   }
 }
