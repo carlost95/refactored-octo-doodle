2c0210ee0d89983e8dd35d854fc6ab78e3064ea1
carlost95 <carlosanibaltejada@gmail.com>
2022-09-24 17:12:57 -0300

    limpieza de archivos u formating in code

--- src/app/pages/logistica/logistica.module.ts
+++ src/app/pages/logistica/logistica.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { LogisticaRoutingModule } from "./logistica-routing.module";
-import { MapaComponent } from "./mapa/mapa.component";
 import { LogisticaComponent } from "./logistica.component";
-import { GoogleMapsModule } from "@angular/google-maps";
 import { ScreenMapComponent } from './screens/screen-map/screen-map.component';
 import { LoadingComponent } from './loading/loading.component';
 import { MapViewComponent } from './map-view/map-view.component';
 import { BtnMyLocationComponent } from './btn-my-location/btn-my-location.component';
-import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
-    MapaComponent,
     LogisticaComponent,
     ScreenMapComponent,
     LoadingComponent,
@@ -22,7 +18,7 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [
     CommonModule,
     LogisticaRoutingModule,
-    GoogleMapsModule],
+  ],
   exports: [
     ScreenMapComponent,
     LoadingComponent,
